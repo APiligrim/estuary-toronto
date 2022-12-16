62646b1a7c3557704f89ec4e0c4c9e6e8aa9c5a8
Anastasiya Uraleva <anastasiyauraleva@Anastasiyas-MacBook-Pro.local>
2022-12-16 00:11:15 -0500

    Added bottom animation

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,10 +1,24 @@
 import styles from '@components/Banner.module.scss';
+import * as U from '@common/utilities';
 
 export default function Banner() {
   return (
-    <div className={styles.banner}>
-      <p className={(styles.primary, styles.scrollingText)}>✦ PARTY.ESTUARY.TECH @ CLIO, TORONTO JANUARY 24,2023 ✦</p>
-      <p className={(styles.secondary, styles.scrollingText)}>✦ PARTY.ESTUARY.TECH @ CLIO, TORONTO JANUARY 24,2023 ✦</p>
+    <div className={styles.marqueeContainer} style={{ position: 'relative' }}>
+      <div className={styles.marquee} style={{ display: 'flex', paddingLeft: '8px' }}>
+        <span className={styles.text}>✦ PARTY . ESTUARY . TECH @ CLIO </span>
+        <span className={styles.text}>TORONTO JANUARY 24, 2023 </span>
+        <span className={styles.text}>✦ PARTY . ESTUARY . TECH @ CLIO </span>
+        <span className={styles.text}>TORONTO JANUARY 24, 2023 </span>
+        <span className={styles.text}>✦ PARTY . ESTUARY . TECH @ CLIO </span>
+      </div>
+
+      <div className={U.classNames(styles.marquee, styles.marquee2)} style={{ display: 'flex', paddingLeft: '8px' }}>
+        <span className={styles.text}>✦ PARTY . ESTUARY . TECH @ CLIO </span>
+        <span className={styles.text}>TORONTO JANUARY 24, 2023 </span>
+        <span className={styles.text}>✦ PARTY . ESTUARY . TECH @ CLIO </span>
+        <span className={styles.text}>TORONTO JANUARY 24, 2023 </span>
+        <span className={styles.text}>✦ PARTY . ESTUARY . TECH @ CLIO </span>
+      </div>
     </div>
   );
 }
